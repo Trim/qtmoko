4047344aa5577b886aecde075a8a33a8e0be9f87
Adrien Dorsaz <adrien@t420s-ad>
2013-03-15 01:34:40 +0100

    Connect EditServer to IcalServer and settings and some fixes
    It fix:
    * maximized widget
    * rename labels/lineEdits
    * emits signal when we close the widget (need check)

--- src/3rdparty/applications/getcal/editserver.cpp
+++ src/3rdparty/applications/getcal/editserver.cpp
@@ -4,8 +4,12 @@ EditServer::EditServer(QWidget *parent, Qt::WFlags f) :
     QWidget(parent, f)
 {
     setupUi(this);
-    show();
     qRegisterMetaType<IcalServer>("IcalServer");
+    _server = new IcalServer();
+    QObject::connect(serverNameLine, SIGNAL(textChanged(QString)), _server, SLOT(setServerName(QString)));
+    QObject::connect(serverAddressLine, SIGNAL(textChanged(QString)), _server, SLOT(setServerAddress(QString)));
+    QObject::connect(userNameLine, SIGNAL(textChanged(QString)), _server, SLOT(setUserName(QString)));
+    QObject::connect(userPassLine, SIGNAL(textChanged(QString)), _server, SLOT(setUserPass(QString)));
 }
 
 EditServer::~EditServer(){
@@ -13,14 +17,22 @@ EditServer::~EditServer(){
 }
 
 void EditServer::addServer(){
-    server = new IcalServer(QString("A unique name for this server"), QString("http://example.org"));
+    _server = new IcalServer(QString("A unique name for this server"), QString("http://example.org"));
     updateUI();
 }
 
 void EditServer::updateUI(){
-    serverNameLineEdit->clear();
-    serverNameLineEdit->insert(server->getServerName());
+    serverNameLine->clear();
+    serverNameLine->insert(_server->getServerName());
 
-    serverUrlLineEdit->clear();
-    serverUrlLineEdit->insert(server->getServerAddress());
+    serverAddressLine->clear();
+    serverAddressLine->insert(_server->getServerAddress());
+    if(!isVisible()){
+        showMaximized();
+    }
+}
+
+bool EditServer::close(){
+    emit endEdit(_server);
+    return true;
 }
